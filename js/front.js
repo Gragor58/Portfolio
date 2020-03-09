0b805641255891e86abdbf9abb9d2f09941307fa
Gragor58 <gragor46@gmail.com>
2020-03-09 09:31:45 +0000

    map mise a jour sur vesoul

--- js/front.js
+++ js/front.js
@@ -81,7 +81,7 @@ $(function () {
 function map() {
 
     var mapId = 'map',
-        mapCenter = [53.14, 8.22],
+        mapCenter = [ 47.619788, 6.154280],
         mapMarker = true;
 
     if ($('#' + mapId).length > 0) {
